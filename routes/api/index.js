992f6fede03205fd63877029d8c3207b38ede8aa
Anthony Castillo <anthonycastillo821@gmail.com>
2021-05-21 13:20:43 -0500

    Adding dummy files for users/events api routes and corresponding controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const eventRoutes = require("./events");
+const userRoutes = require("./users");
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use("/events", eventRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
