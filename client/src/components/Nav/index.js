3c86c1b58031b3acdd2c7acd05b29daa3fa9981c
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-22 21:53:00 -0500

    added base level for log in page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Nav() {
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         React Reading List
       </a>
+    
     </nav>
   );
 }
