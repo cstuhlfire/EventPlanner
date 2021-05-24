a5c6363b061e8e7730ce777a52a12dd7cf41c04f
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-05-24 10:15:53 -0500

    edited events, seed data, to populate from users for lists, attendees, announcements, comments

--- scripts/testDB.js
+++ scripts/testDB.js
@@ -10,9 +10,50 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mulletevents", 
   useFindAndModify: false });
 
 
-db.Events.find({}).populate("attendees.attendee")
-.then(dbEvents => {
-  dbEvents.forEach((event) => console.log(event.attendees));
+// db.Events.find({}).populate("attendees.attendee")
+// .then(dbEvents => {
+//   let result = dbEvents.map((event) => 
+//     (event.attendees).map(((guest) => guest.attendee.username)));
+//   console.log(result);
+//   process.exit(0);
+// })
+// .catch(err => {
+//   console.log(err);
+//   process.exit(1);
+// });
+
+// db.Lists.findOne({})
+// .then(lists => {
+
+//   // let test = lists.map((list) => list.items)
+//   let test = lists.items.map((itm) => itm);
+//   console.log(test);
+
+//   process.exit(0);
+// })
+// .catch(err => {
+//   console.log(err);
+//   process.exit(1);
+// });
+
+db.Events.findOne()
+  .populate({
+    path: "lists.items",
+    populate: "assignedTo"
+  })
+  .populate("attendees.attendee")
+  .populate("announcements.author")
+  .populate("comments.author")
+  
+ .then(dbEvents => {
+
+    console.log(dbEvents);
+    dbEvents.lists.forEach((list) => console.log(list.items));
+    dbEvents.attendees.forEach((attendee) => console.log(attendee));
+    dbEvents.announcements.forEach((announcement) => console.log(announcement));
+    dbEvents.comments.forEach((comment) => console.log(comment));
+
+   
   process.exit(0);
 })
 .catch(err => {
