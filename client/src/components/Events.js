9659820d4cea607e387e93f5f9e1b1e681c230e5
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-24 13:15:33 -0500

    added events component and home page

---
+++ client/src/components/Events.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Events.css"
+
+function Events() {
+    return (
+    <div>
+        <div class="columns">
+        <div class="column">
+            <figure className="columns image is-128x128">
+                <img className="image" src="https://bulma.io/images/placeholders/128x128.png"></img>
+            </figure>
+        </div>
+        <div class="column">
+        <div className="columns content">
+           <p>party.description</p>
+        </div>
+        </div>
+        </div>
+    </div>
+    )
+}
+
+export default Events
