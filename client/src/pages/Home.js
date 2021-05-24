ba9425824ad38321106e45e752233fc19f27a031
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-24 10:18:43 -0500

    added a home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import "./Home.css"
 
 
 function Home() {
     return (
         <div>
-           
-            <h1>Test</h1>
+            <div className="testimonial">
+               <h1 className="p">Business in the Front, Party in the Back</h1>
+               <p className="p">Create an Event or Join one here! </p>
+            </div>
         </div>
     )
 }
--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -6,11 +6,10 @@ function Navbar() {
         <div>
             <nav className="navbar" role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
-                   
                     <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
                     </a>
                 </div>
                 <div id="navbarBasicExample" className="navbar-menu">
