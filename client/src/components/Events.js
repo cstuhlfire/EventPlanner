51f6c3a920c6ec50b6a7352b123ca88c93378684
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-24 21:35:23 -0500

    fixed styling error on input tag

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import "./Events.css"
+import AddIcon from '@material-ui/icons/Add';
 
 function Events() {
     return (
     <div>
-        <div class="columns">
-        <div class="column">
-            <figure className="columns image is-128x128">
-                <img className="image" src="https://bulma.io/images/placeholders/128x128.png"></img>
-            </figure>
-        </div>
-        <div class="column">
-        <div className="columns content">
-           <p>party.description</p>
-        </div>
-        </div>
+        <div class="cc columns features">
+        <div class="column is-half">
+          <div class="card card is-shady">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="https://source.unsplash.com/RWnpyGtY1aU" alt="Placeholder image" class="modal-button" data-target="modal-image2"></img>
+              </figure>
+            </div>
+            <div>
+              <button className="joinBtn">
+                <AddIcon></AddIcon>
+              </button>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                <h4> props.title</h4>
+                <p> props.description</p>
+                <span class="button is-link modal-button" data-target="modal-image2">View Event</span>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
+</div>
     )
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import "./Home.css"
 import Events from "../components/Events"
+import SearchIcon from '@material-ui/icons/Search';
+
 
 
 function Home() {
     return (
         <div>
             <div className="testimonial">
-               <h1 className="p">Business in the Front, Party in the Back</h1>
+            <svg>
+              <text x="50%" y="60%"  text-anchor="middle">
+               Its Party Time
+              </text>
+            </svg>
                <p className="p">Create an Event or Join one here! </p>
             </div>
-        <Events />
+            <div className="search">
+                <input className=" i input is-primary" type="text" placeholder="Search Event">
+                </input>
+             <button className="bt is-left">
+               <SearchIcon></SearchIcon>               
+            </button>
+            </div>
+
+         {/* map event component here */}
+
+        <Events 
+        // title={api.title}
+        // image={api.image} 
+        // description={api.description} 
+        />
         </div>
     )
 }
