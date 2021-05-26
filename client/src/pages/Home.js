ca62e25564ff7c2f607637c7cb082f8bc8fec745
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-26 18:09:08 -0500

    added modal to to-do list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -37,32 +37,17 @@ function Home() {
             </div>
 
       <div>
-          <div className="testimonial">
-          <svg>
-            <text x="50%" y="60%"  text-anchor="middle">
-              Its Party Time
-            </text>
-          </svg>
-              <p className="p">Create an Event or Join one here! </p>
-          </div>
-          <div className="search">
-              <input className=" i input is-primary" type="text" placeholder="Search Event">
-              </input>
-            <button className="bt is-left">
-              <SearchIcon></SearchIcon>               
-          </button>
-          </div>
-
+         
         {/* map event component here */}
         <div className="column">
-            { events.length > 0 && events.map(event =>
+            {/* { events.length > 0 && events.map(event => */}
                 <Events 
-                title={event.title}
-                date={event.date}
-                time={event.time}
-                location={event.location}
+                // title={title}
+                // date={date}
+                // time={time}
+                // location={location}
                 />
-            )}
+            {/* )} */}
         </div>
       </div>
     </div>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,47 +7,6 @@ export default {
     return axios.post(API_URL + "/api/users", userData);
   },
   checkUser: function(userData){
-<<<<<<< HEAD
-    return axios.post(API_URL + "/api/users/login", userData)
-  },
-  createEvent: function(eventData) {
-    return axios.post(API_URL + "/api/events", eventData)
-  },
-  createList: function(listData) {
-    return axios.post(API_URL + "/api/events", listData)
-  },
-  getEvents: function(){
-    return axios.get(API_URL + "/api/events");
-  },
-  getOneEvent: function(id) {
-    return axios.get(API_URL + "/api/events/" + id);
-  },
-  deleteEvent: function(id) {
-    return axios.delete(API_URL + "/api/events/" + id);
-  },
-  deleteList: function (id, listName) {
-    return axios.delete(API_URL + "/api/events/" + id + "/list/" + listName)
-  },
-  removeAttendee: function (id, username) {
-    return axios.delete(API_URL + "/api/events/" + id + "/attendee/" + username)
-  },
-  removeAnnouncement: function (id, announcementid) {
-    return axios.delete(API_URL + "/api/events/" + id + "/announcement/" + announcementid)
-  },
-  getLists: function(event_id){
-    return axios.get(API_URL + "/api/lists/" + event_id);
-  },
-  getActions: function(list_id){
-    return axios.get(API_URL + "/api/actions/" + list_id);
-  },
-  getAnnouncements: function(event_id){
-    return axios.get(API_URL + "/api/announcements/" + event_id);
-  },
-  getAttendees: function(event_id){
-    return axios.get(API_URL + "/api/attendees/" + event_id);
-  }
-};
-=======
     return axios.post("/api/users/login", userData)
   },
   createEvent: function(eventData) {
@@ -87,4 +46,3 @@ export default {
   //   return axios.get("/api/attendees/" + event_id);
   // }
 };
->>>>>>> 211e548a40e1d297400148d3855548c62983e67f
