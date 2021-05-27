bdd9b495ce4c44ae168f2815793e2fab2907de0f
Anthony Castillo <anthonycastillo821@gmail.com>
2021-05-27 02:10:04 -0500

    Create event with api call working

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,8 +1,46 @@
 import React from 'react'
 import "./CreateEvent.css"
+import API from "../utils/API";
+
 
 
 function CreateEvent() {
+
+    function saveEvent (event){
+        event.preventDefault();
+
+        let nameinput = document.querySelector(`#eventName`).value;
+        let locationinput = document.querySelector(`#location`).value;
+        let dateinput = document.querySelector(`#date`).value;
+        let timeinput = document.querySelector(`#time`).value;
+        let descinput = document.querySelector(`#description`).value;
+        let imageinput = document.querySelector(`#image`).value;
+        
+        let dateTimeinput = `${dateinput} ${timeinput}`
+        console.log(nameinput)
+        console.log(locationinput)
+        console.log(dateinput) 
+        console.log(dateTimeinput) 
+        console.log(timeinput) 
+        console.log(descinput)
+        console.log(imageinput)
+
+        API.createEvent({
+            eventName: nameinput,
+            location: locationinput,
+            description: descinput,
+            eventDateTime: dateTimeinput,
+            lists: [],
+            announcements: [],
+            comments: [],
+            attendees: []
+
+        })
+
+
+    }
+
+
     return (
     <div className="container">
         <div className="h1">
@@ -11,27 +49,27 @@ function CreateEvent() {
         <div className="field">
         <label className="name label">Event Name</label>
         <div className="control">
-            <input className="input" type="text" placeholder="Event Name"></input>
+            <input className="input" type="text" placeholder="Event Name" id="eventName"></input>
         </div>
         </div>
 
         <div className="field">
         <label className="label">Event Location</label>
         <div className="control">
-            <input className="input" type="address" placeholder="Address" ></input>
+            <input className="input" type="address" placeholder="Address" id="location"></input>
         </div>
         </div>
 
         <div className="field">
         <label className="label">Date</label>
         <div className="control">
-            <input className="input" type="date" placeholder="Date" ></input>
+            <input className="input" type="date" placeholder="Date" id="date"></input>
         </div>
         </div>
         <div className="field">
         <label className="label">Time</label>
         <div className="control">
-            <input className="input" type="time" ></input>
+            <input className="input" type="time" id="time"></input>
         </div>
         </div>
 
@@ -39,12 +77,12 @@ function CreateEvent() {
         <label className="label">Image</label>
         <div className="control">
             <div className="select">
-            <select>
+            <select id="image">
                 <option>Select dropdown</option>
-                <option>Get together</option>
-                <option>Birthday</option>
-                <option>Graduation</option>
-                <option>Wedding</option>
+                <option value="confetti">Get together</option>
+                <option value="balloons">Birthday</option>
+                <option value="pool">Graduation</option>
+                <option value="balloons">Wedding</option>
             </select>
             </div>
         </div>
@@ -53,12 +91,12 @@ function CreateEvent() {
         <div className="field">
         <label className="label">Event Description</label>
         <div className="control">
-            <textarea className="textarea" placeholder="Textarea"></textarea>
+            <textarea className="textarea" placeholder="Textarea" id="description"></textarea>
         </div>
         </div>
         <div className="field is-grouped">
         <div className="btnCont control">
-            <button className="subbttn button is-link">Submit</button>
+            <button className="subbttn button is-link" onClick={saveEvent}>Submit</button>
         </div>
         </div>   
     </div>
